1d9360cfa9c686325467466019fc35f3f5669975
samie820 <samuelomole70@gmail.com>
2019-06-09 20:02:09 +0100

    Add some extra objects to the song list

--- models/Song.js
+++ models/Song.js
@@ -1,35 +1,100 @@
-const songs = [{
-  id: 1,
-  name: "test1",
-  "albumArt": "test",
-  "songUrl": "test",
-  "rating": 5,
-  "description": "testing..."
-},
-{
-  id: 2,
-  name: "test1",
-  "albumArt": "test",
-  "songUrl": "test",
-  "rating": 2,
-  "description": "testing..."
-},
-{
-  id: 3,
-  name: "test1",
-  "albumArt": "test",
-  "songUrl": "test",
-  "rating": 3,
-  "description": "testing..."
-},
-{
-  id: 4,
-  name: "test1",
-  "albumArt": "test",
-  "songUrl": "test",
-  "rating": 1,
-  "description": "testing..."
-}
+const songs = [
+  {
+    id: 1,
+    name: "LOST IN TIME",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029436/albumart2.png",
+    artist: "HOOKED",
+    rating: 5
+  },
+  {
+    id: 2,
+    name: "BACK TO THE FUTURE",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029429/albumart5.jpg",
+    artist: "HOOKED",
+    rating: 2
+  },
+  {
+    id: 3,
+    name: "MIDDLE CHILD",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029429/albumart1.jpg",
+    artist: "HOOKED",
+    rating: 3
+  },
+  {
+    id: 4,
+    name: "REIGN",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029417/albumart6.jpg",
+    artist: "HOOKED",
+    rating: 1
+  },
+  {
+    id: 5,
+    name: "THE CLICK",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029416/albumart4.jpg",
+    artist: "HOOKED",
+    rating: 1
+  },
+  {
+    id: 6,
+    name: "VISION OF DREAMS",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029412/albumart3.jpg",
+    artist: "HOOKED",
+    rating: 1
+  },
+  {
+    id: 7,
+    name: "LOST IN TIME",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029436/albumart2.png",
+    artist: "HOOKED",
+    rating: 5
+  },
+  {
+    id: 8,
+    name: "BACK TO THE FUTURE",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029429/albumart5.jpg",
+    artist: "HOOKED",
+    rating: 2
+  },
+  {
+    id: 9,
+    name: "MIDDLE CHILD",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029429/albumart1.jpg",
+    artist: "HOOKED",
+    rating: 3
+  },
+  {
+    id: 10,
+    name: "REIGN",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029417/albumart6.jpg",
+    artist: "HOOKED",
+    rating: 1
+  },
+  {
+    id: 11,
+    name: "THE CLICK",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029416/albumart4.jpg",
+    artist: "HOOKED",
+    rating: 1
+  },
+  {
+    id: 12,
+    name: "VISION OF DREAMS",
+    albumArt:
+      "https://res.cloudinary.com/schms/image/upload/v1560029412/albumart3.jpg",
+    artist: "HOOKED",
+    rating: 1
+  }
 ];
 
 module.exports = songs;
